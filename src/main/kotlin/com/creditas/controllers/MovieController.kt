72f0e3061f847995d18cf11a12097926a4b637ff
oricis <m_alc1@hotmail.com>
2019-10-28 13:01:07 +0100

    created array with movies & basic query methods

--- src/main/kotlin/com/creditas/controllers/MovieController.kt
+++ src/main/kotlin/com/creditas/controllers/MovieController.kt
@@ -8,20 +8,63 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class MovieController
 {
+    val movies: Array<Movie> = arrayOf(
+            Movie(
+                    1,
+                    "Soy Leyenda",
+                    "Foo The Director",
+                    2012,
+                    40.00F,
+                    true
+            ),
+            Movie(
+                    2,
+                    "Soy Foo",
+                    "Foo The Director",
+                    2015,
+                    36.00F,
+                    false
+            ),
+            Movie(
+                    3,
+                    "Hace Calor",
+                    "The Torrefactor",
+                    2009,
+                    36.00F,
+                    false
+            ),
+            Movie(
+                    4,
+                    "The X",
+                    "John Doe",
+                    2011,
+                    42.00F,
+                    false
+            )
+    );
 
     @GetMapping ("/movies")
+
     fun getAllMovies(
-        @RequestParam(value = "name", defaultValue = "all")
-        name: String
-    )
+            @RequestParam(value = "name", defaultValue = "all")
+            name: String
+    ): Array<Movie>
     {
-        Movie(
-                1,
-                "Soy Leyenda",
-                "Foo The Director",
-                2012,
-                40.00F,
-                true
-        );
+        return movies;
+    }
+
+    @GetMapping ("/movie")
+    fun getMovie(
+            @RequestParam(value = "id", defaultValue = "0")
+            id: Int
+    ): Movie?
+    {
+        for (movie in movies){
+            if (movie.id.equals(id)) {
+                return movie;
+            }
+        }
+
+        return null;
     }
 }
