05eaef39208db2f17006736735eec64b710f9e9f
oricis <m_alc1@hotmail.com>
2019-10-28 13:00:22 +0100

    added main class

---
+++ src/main/kotlin/com/creditas/CreditasApplication.kt
@@ -0,0 +1,11 @@
+package com.creditas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditasApplication
+
+fun main(args: Array<String>) {
+    runApplication<CreditasApplication>(*args)
+}
